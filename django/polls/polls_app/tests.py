fd653a89b537cbb1c8e04c329e7058610650c0da
Kraig Ochieng <kraigochieng@gmail.com>
2023-07-02 16:50:57 +0300

    finished with static files

--- django/polls/polls_app/tests.py
+++ django/polls/polls_app/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.urls import reverse
+import datetime
+from django.utils import timezone
 
+from .models import Question
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    time =timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls_app:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls available")
+        self.assertQuerysetEqual(response.context['question'], [])
+
+def test_past_question(self):
+    question = create_question(question_text="Past question.", days=-30)
+    response = self.client.get(reverse("polls:index"))
+    self.assertQuerySetEqual(response.context["question"],[question],)
+
+def test_future_question(self):
+    question = create_question(question_text="Future question.", days=30)
+    response = self.client.get(reverse("polls:index"))
+    self.assertContains(response, "No polls are available.")
+    self.assertQuerysetEqual(response.context["question"], [question])
+
+def test_future_question_and_past_question(self):
+    question1 = create_question(question_text="Past question.", days=-30)
+    question2 = create_question(question_text="Future question.", days=30)
+    response = self.client.get(reverse("polls:index"))
+    self.assertQuerySetEqual(response.context["question"],[question1, question2],)
+
+def test_two_past_questions(self):
+    question1 = create_question(question_text="Past question 1.", days=-30)
+    question2 = create_question(question_text="Past question 2.", days=-5)
+    response = self.client.get(reverse("polls:index"))
+    self.assertQuerySetEqual(response.context["question"],[question2, question1],)
--- django/polls/polls_app/views.py
+++ django/polls/polls_app/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from typing import Any
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.http import  HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world, Polls index")
+class IndexView(generic.ListView):
+    template_name = "polls_app/index.html"
+    context_object_name = "questions"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls_app/detail.html"
+
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = "polls_app/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        error_message = "You did not selecta choice"
+        context = {
+            'question': question,
+            'error_message': error_message,
+        }
+        return render(request, "polls_app/detail.html", context)
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse("polls_app:results", args=(question.id,)))
